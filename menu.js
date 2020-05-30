b6ffdf94659ad450b40818255e8285718efad852
andre-lazo <65989412+andre-lazo@users.noreply.github.com>
2020-05-29 19:20:37 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,19 @@
+$(document).ready(main);// se ejecuatra cuando cargue la pantalla
+var contador=1;
+function main(){
+    $('.menu_bar').click(function(){
+//$('nav').toggle();
+if (contador==1){
+    $('nav').animate({
+        left:'0'
+    });
+    contador=0;
+}else{
+    $('nav').animate({
+        left:'-100%'
+    });
+    contador=1;
+}
+    });
+
+}
